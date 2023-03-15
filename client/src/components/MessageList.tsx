c09a4fefd119ada4862b6a990091119821b6d184
Jacksonmills <jacksonemills@gmail.com>
2023-03-15 14:33:41 -0500

    made messages more clean and functional, scrollable

--- client/src/components/MessageList.tsx
+++ client/src/components/MessageList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 export type MessageListProps = {
@@ -9,27 +10,88 @@ export type MessageListProps = {
   }[];
 };
 
-const MessageList = ({ messages }: MessageListProps) => (
-  <Wrapper>
-    {messages.map((message, index) => {
-      return message.isServerMessage ? (
-        <ConnectedEventMessage key={index}>
-          <Username hexcode={message.hexcode}>{message.username}</Username> {message.content}
-        </ConnectedEventMessage>
-      ) : (
-        <Message key={index}>
-          <Username hexcode={message.hexcode}>{message.username}:</Username>
-          {message.content}
-        </Message>
-      );
-    })}
-  </Wrapper>
-);
+const MessageList = ({ messages }: MessageListProps) => {
+  const [showScrollToBottom, setShowScrollToBottom] = useState(false);
+  const [userScrolled, setUserScrolled] = useState(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
-const Wrapper = styled.ul`
+  const handleScroll = () => {
+    if (!wrapperRef.current) return;
+
+    const { scrollTop, scrollHeight, clientHeight } = wrapperRef.current;
+    const atBottom = scrollHeight - scrollTop === clientHeight;
+
+    setShowScrollToBottom(!atBottom);
+    setUserScrolled(true);
+  };
+
+  const scrollToBottom = () => {
+    setUserScrolled(false);
+    setTimeout(() => {
+      if (wrapperRef.current) {
+        wrapperRef.current.scrollTo(0, wrapperRef.current.scrollHeight);
+      }
+    }, 50);
+  };
+
+  useEffect(() => {
+    if (wrapperRef.current) {
+      wrapperRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (wrapperRef.current) {
+        wrapperRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    setUserScrolled(false);
+    scrollToBottom();
+  }, [messages]);
+
+  return (
+    <Wrapper ref={wrapperRef}>
+      <List>
+        {messages.map((message, index) => {
+          return message.isServerMessage ? (
+            <ServerEventMessage type="connected" key={index}>
+              <Username hexcode={message.hexcode}>{message.username}</Username> {message.content}
+            </ServerEventMessage>
+          ) : (
+            <Message key={index}>
+              <Username hexcode={message.hexcode}>{message.username}:</Username>
+              {message.content}
+            </Message>
+          );
+        })}
+      </List>
+      {showScrollToBottom && userScrolled && (
+        <ScrollToBottomButton onClick={scrollToBottom}>Newer messages</ScrollToBottomButton>
+      )}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+  scroll-behavior: smooth;
+`;
+
+const List = styled.ul`
   list-style: none;
   padding: 0;
   width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
 `;
 
 const Username = styled.span<{ hexcode?: string; }>`
@@ -43,12 +105,21 @@ const Message = styled.li`
   max-width: 100vw;
 `;
 
-const ConnectedEventMessage = styled(Message)`
-  color: green;
+const ServerEventMessage = styled(Message) <{ type: "connected" | "disconnected", }>`
   padding: 12px;
   background: #333;
+  border: 1px solid ${props => props.type === "connected" ? "green" : "red"};
   border-radius: .25rem;
   margin: 6px 12px;
 `;
+
+const ScrollToBottomButton = styled.button`
+  position: absolute;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 5px 10px 6px;
+`;
+
 
 export default MessageList;
