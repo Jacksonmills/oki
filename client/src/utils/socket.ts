78af48b46d2d31fec2dcf6014c6724b5f2784909
Jacksonmills <jacksonemills@gmail.com>
2023-03-15 00:58:24 -0500

    updated socket for production

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,3 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:3001');
+const isDevelopment = process.env.NODE_ENV === 'development';
+const serverURL = isDevelopment ? 'http://localhost:3001' : window.location.origin;
+
+export const socket = io(serverURL);
