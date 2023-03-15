eae1b0a912deadc1c217767dc95ac87c3d702488
Jacksonmills <jacksonemills@gmail.com>
2023-03-15 00:44:25 -0500

    trying with new builds

--- server/server.ts
+++ server/server.ts
@@ -12,10 +12,9 @@ const PORT = process.env.PORT || 3001;
 
 const generateRandomUsername = () => `User${Math.floor(Math.random() * 10000)}`;
 
-const clientPath = path.join(__dirname, '../client/build');
-app.use(express.static(clientPath));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(clientPath, 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 io.on('connection', (socket) => {
