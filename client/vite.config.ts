2b6688ee9678a3339083a010e2aaa172ac8b8573
Jacksonmills <jacksonemills@gmail.com>
2023-04-08 07:20:39 -0500

    minimal PWA setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'oki.svg'],
+      manifest: {
+        name: 'OKI',
+        short_name: 'OKI',
+        description: 'Anonymous, temporary, Twitch-like chat experiences for live events.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
   build: {
     outDir: 'build',
   },
