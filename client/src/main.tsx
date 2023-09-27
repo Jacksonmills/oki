16b1e3e19fd1ee9bf44a69a2525b300c23d6387d
Jacksonmills <jacksonemills@gmail.com>
2023-09-27 08:36:37 -0500

    Remove unnecessary routing code in main.tsx file
    - The routing code related to the "/room" path has been simplified.
    - The inner route for an empty path has been removed, while still preserving the route for individual room IDs.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import { UserProvider } from './UserContext';
 import { MessageProvider } from './MessageContext';
 import GlobalStyles from './components/GlobalStyles';
 import { LevelingProvider } from './LevelingContext';
-import { BrowserRouter, Navigate, Route, Routes, useParams } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
 import ChatRoom from './ChatRoom';
 
 function RoomPage() {
@@ -25,7 +25,6 @@ const Root = () => {
               <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/room">
-                  <Route path="" element={<Navigate to="/room/public" replace={true} />} />
                   <Route path=":roomId" element={<RoomPage />} />
                 </Route>
               </Routes>
